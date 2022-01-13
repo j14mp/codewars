de56480539c7a9bd35dc9eb350942007e9546fe9
j14mp <97001372+j14mp@users.noreply.github.com>
2022-01-12 20:07:53 -0800

    Create quarter_of_year.py

---
+++ 8kyu/quarter_of_year.py
@@ -0,0 +1,18 @@
+"""
+Given a month as an integer from 1 to 12, return to which quarter of the year it belongs as an integer number.
+
+For example: month 2 (February), is part of the first quarter; month 6 (June), is part of the second quarter; and month 11 (November), is part of the fourth quarter.
+"""
+def quarter_of(month):
+  first = [1, 2, 3]
+  second = [4, 5, 6]
+  third = [7, 8, 9]
+  fourth = [10, 11, 12]
+  if month in first:
+    return 1
+  elif month in second:
+    return 2
+  elif month in third:
+    return 3
+  elif month in fourth:
+    return 4
