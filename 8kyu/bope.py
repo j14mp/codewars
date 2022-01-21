99b95df6c8c5c735351f7ecfb8db91861a41530a
j14mp <97001372+j14mp@users.noreply.github.com>
2022-01-20 17:24:01 -0800

    Create bope.py

---
+++ 8kyu/bope.py
@@ -0,0 +1,27 @@
+"""
+The BOPE is the squad of special forces of police that usually handles the operations in the Favelas in Rio de Janeiro.
+
+In this Kata you have to write a function that determine the number of magazines that every soldier has to have in his bag.
+
+You will receive the weapon and the number of streets that they have to cross. Considering that every street the officer shoots 3 times. Bellow there is the relation of weapons:
+
+PT92 - 17 bullets
+M4A1 - 30 bullets
+M16A2 - 30 bullets
+PSG1 - 5 bullets
+
+Example:
+
+["PT92", 6] => 2 (6 streets 3 bullets each)
+["M4A1", 6] => 1
+
+The return Will always be an integer so as the params.
+"""
+from typing import Tuple
+import math
+
+def mag_number(info: Tuple[str, int]) -> int:
+    specs = {'PT92' : 17, 'M4A1' : 30, 'M16A2' : 30, 'PSG1' : 5}
+    mag_capacity = specs[info[0]]
+    bullets_used = info[1] * 3
+    return 1 if bullets_used <= mag_capacity else math.ceil(bullets_used / mag_capacity)
