3a0e20f159a2e5bef2416c8811d8f46c7b26e57e
j14mp <97001372+j14mp@users.noreply.github.com>
2022-01-27 19:06:54 -0800

    Create basic_math_ops.py

---
+++ 8kyu/basic_math_ops.py
@@ -0,0 +1,23 @@
+"""
+Description:
+Your task is to create a function that does four basic mathematical operations.
+
+The function should take three arguments - operation(string/char), value1(number), value2(number).
+The function should return result of numbers after applying the chosen operation.
+
+Examples(Operator, value1, value2) --> output
+('+', 4, 7) --> 11
+('-', 15, 18) --> -3
+('*', 5, 5) --> 25
+('/', 49, 7) --> 7
+"""
+
+import operator
+def basic_op(oper, value1, value2):
+    oper_dict = {
+        '+' : operator.add,
+        '-' : operator.sub,
+        '*' : operator.mul,
+        '/' : operator.truediv
+    }
+    return oper_dict[oper](value1, value2)
